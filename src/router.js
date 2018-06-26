46f96a8870af2df4f9e5a52574ff3cbcaec0887c
maye-msft <34230764+maye-msft@users.noreply.github.com>
2018-06-26 13:47:41 +0800

    fix nodejs deployment issue

--- src/router.js
+++ src/router.js
@@ -1,7 +1,31 @@
 var express = require('express');
+var crypto = require('crypto');
+var axios = require('axios');
+var util = require('./util.js');
+var stringify = require('json-stringify-safe');
 var router = express.Router();
+
+
 router.get('/hello', function (req, res) {
-    res.send(`hello ${new Date()}`)
+    
+    
+    res.send("hello")
+
 });
+
+
+router.post('/detectface', function (req, res) {
+    
+    
+    util.detectface(req.body,(response)=>{
+        res.json(response)
+    }, (error)=>{
+        res.send(JSON.parse(stringify(error)))
+        console.log(error)
+    })
+
+});
+
+
 
 module.exports = router;
